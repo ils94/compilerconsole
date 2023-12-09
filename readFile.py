49ea05469275b0dd279b67532ef9013b743c26b2
none <none@>
2023-12-09 09:22:55 -0300

    quick fix

--- readFile.py
+++ readFile.py
@@ -1,6 +1,11 @@
+import os
+
+
 def read_last_python_path(entry):
-    # Open the file in read mode
-    with open("last_python_path", "r") as file:
-        # Read the contents of the file
-        file_contents = file.read()
-        entry.insert(0, file_contents.lstrip())
+    path = "last_python_path"
+
+    if os.path.isfile(path):
+        with open(path, "r") as file:
+            # Read the contents of the file
+            file_contents = file.read()
+            entry.insert(0, file_contents.lstrip())
